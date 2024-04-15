dc10adde95d11d939eb63d27d329881365677746
Сергей <165053016+Cthutqas@users.noreply.github.com>
2024-04-15 20:37:58 +0300

    Add files via upload

---
+++ module_4.1.py
@@ -0,0 +1,20 @@
+def test_function():
+    print('Проверка')
+
+    def inner_function():
+        print('Я в области видимости функции test_function')
+
+    inner_function()
+
+    inner_function()
+
+test_function()
+
+
+
+
+
+
+
+
+
