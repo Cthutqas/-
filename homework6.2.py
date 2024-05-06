51205b28e3fbdf2502409d3bd023410770a91d8d
Сергей <165053016+Cthutqas@users.noreply.github.com>
2024-05-06 14:37:23 +0300

    Add files via upload

---
+++ homework6.2.py
@@ -0,0 +1,32 @@
+# Создайте родительский(базовый) класс Vehicle, который имеет свойство vehicle_type = "none"
+# Создайте родительский(базовый) класс Car, который имеет свойство price = 1000000 и функцию
+# def horse_powers, которая возвращает количество лошидиных сил для автомобиля
+# Создайте наследника класса Car и Vehicle - класс Nissan и переопределите свойство price и vehicle_type,
+# а также переопределите функцию horse_powers
+# Создайте экзмепляр класса Nissan и распечайте через функцию print vehicle_type, price
+
+class Vehicle ():
+    def __init__(self):
+        self.vehicle_type = "none"
+
+class Car ():
+    def __init__(self):
+        self.prise = 1000000
+    def horse_powers (self):
+        self.PowerOfCars = 190
+
+class Nissan (Vehicle, Car):
+    def __init__(self):
+        self.vehicle_type = "off-road vehicle"
+        self.prise = 1058930
+    def horse_powers(self):
+        self.PowerOfCars = 200
+
+car = Nissan()
+print('Ниссан')
+print('мощность', car.horse_powers)
+print('тип', car.vehicle_type)
+print('цена', car.prise)
+
+
+
