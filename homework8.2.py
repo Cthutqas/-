b99176904874be452a493f796f16c34f28f935b2
Сергей <165053016+Cthutqas@users.noreply.github.com>
2024-06-25 21:59:14 +0300

    Add files via upload

--- homework8.2.py
+++ homework8.2.py
@@ -7,36 +7,39 @@
 # В основной части программы вызовите эти функции и корректно обработай
 class InvalidDataException(Exception):
     def __init__(self, message_1, extra_info_1):
-        self.message_1 = message_1
+        super().__init__(message_1)
         self.extra_info_1 = extra_info_1
 
 
 class ProcessingException(Exception):
     def __init__(self, message_2, extra_info_2):
-        self.message_2 = message_2
+        super().__init__(message_2)
         self.extra_info_2 = extra_info_2
 
 list = [2, 45, 12, 56, 8, 9, 31, 58, 345, 13, 67, 95]
 
-def f():
+def f(a):
     return a + 3
+
+
+def f_1(a):
+    if a == 9:
+        raise InvalidDataException('мое исключение 1', 'extra_info_1')
+        # print(f'Мое исключение', 'не верные данные')
+    if a == 13:
+        raise ProcessingException('мое исключение 2', 'extra_info_2')
+        # print('И это мое исключение', ' просто не люблю эту цифру')
+
 for a in list:
 
-    def f_1(a):
-        if a == 9:
-            raise InvalidDataException(Exception)
-        print(f'Мое исключение', 'не верные данные')
-        if a == 13:
-            raise ProcessingException(Exception)
-        print('И это мое исключение', ' просто не люблю эту цифру')
     try:
-        f()
+        f_1(a)
     except InvalidDataException as e_1:
         print('Сообщение об ошибке:', e_1)
-    except InvalidDataException as e_2:
+    except ProcessingException as e_2:
         print(f'Сообщение об ошибке:', e_2)
-    rezult = f()
-    print(rezult)
-else:
-    print('задача выполнена')
+    else:
+        rezult = f(a)
+        print(rezult)
+print('задача выполнена')
 
