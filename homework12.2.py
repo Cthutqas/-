14abeffb447692f0823de1b4cfb86b44cfb8c3c5
Сергей <165053016+Cthutqas@users.noreply.github.com>
2024-07-25 14:22:03 +0300

    Add files via upload

---
+++ homework12.2.py
@@ -0,0 +1,107 @@
+# В первую очередь скачайте исходный код, который нужно обложить тестами с GitHub. (Можно скопировать)
+# В этом коде сможете обнаружить дополненный с предыдущей задачи класс Runner и новый класс Tournament.
+# Изменения в классе Runner:
+# Появился атрибут speed для определения скорости бегуна.
+# Метод __eq__ для сравнивания имён бегунов.
+# Переопределены методы run и walk, теперь изменение дистанции зависит от скорости.
+# Класс Tournament представляет собой класс соревнований, где есть дистанция, которую нужно пробежать и
+# список участников. Также присутствует метод start, который реализует логику бега по предложенной дистанции.
+#
+# Напишите класс TournamentTest, наследованный от TestCase. В нём реализуйте следующие методы:
+#
+# setUpClass - метод, где создаётся атрибут класса all_results. Это словарь в который будут сохраняться результаты
+# всех тестов.
+# setUp - метод, где создаются 3 объекта:
+# Бегун по имени Усэйн, со скоростью 10.
+# Бегун по имени Андрей, со скоростью 9.
+# Бегун по имени Ник, со скоростью 3.
+# tearDownClass - метод, где выводятся all_results по очереди в столбец.
+#
+# Так же методы тестирования забегов, в которых создаётся объект Tournament на дистанцию 90. У объекта класса
+# Tournament запускается метод start, который возвращает словарь в переменную results. В конце вызывается метод
+# assertTrue, в котором сравниваются последний объект из result и предполагаемое имя последнего бегуна (индекс -1).
+# Напишите 3 таких метода, где в забегах участвуют (порядок передачи в объект Tournament соблюсти):
+# Усэйн и Ник
+# Андрей и Ник
+# Усэйн, Андрей и Ник.
+# Как можно понять: Ник всегда должен быть последним.
+
+import unittest
+
+class Runner:
+    def __init__(self, name, speed=5):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+class TournamentTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runer_1 = Runner('Усэйн', 10)
+        self.runer_2 = Runner('Андрей', 9)
+        self.runer_3 = Runner('Ник', 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for test_key, test_value in cls.all_results.items():
+            print(f'Тест: {test_key}')
+            for key, value in test_value.items():
+                print(f'\t{key}: {value.name}')
+
+    def test_turn1(self):
+
+        turn_1 = Tournament(90, self.runer_1, self.runer_3)
+        result = turn_1.start()
+        print(result[list(result.keys())[-1]] == 'Ник')
+        self.all_results['test_turn1'] = result
+
+    def test_turn2(self):
+        turn_2 = Tournament(90, self.runer_2, self.runer_3)
+        result = turn_2.start()
+        self.all_results['test_turn2'] = result
+
+    def test_turn3(self):
+        turn_3 = Tournament(90, self.runer_1, self.runer_2, self.runer_3)
+        result = turn_3.start()
+        self.all_results['test_turn3'] = result
+if __name__ == '__main__':
+    unittest.main
+
